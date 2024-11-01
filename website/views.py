aed33fa0564bff4e91e25dd593102fbdb88af9fd
Pierre <pierre.nogaro@gmail.com>
2024-11-01 14:41:17 +0100

    choose random word

--- website/views.py
+++ website/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+import spacy
+import random
+
+nlp = spacy.load("en_core_web_lg")
+
+words = [
+    "apple", "book", "cat", "dog", "elephant", "flower", "guitar", "house", "island", "jungle",
+    "kangaroo", "lemon", "mountain", "notebook", "orange", "piano", "queen", "river",
+    "sun", "tree", "umbrella", "violin", "window", "xylophone", "yacht", "zebra"
+]
+
+SECRET_WORD = random.choice(words)
+
+
+
 
 def games_page(request):
     return render(request, 'website/game/game_page.html')
