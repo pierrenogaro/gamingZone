4440b3cab17b846cb754468a15153131e70acb86
Pierre <pierre.nogaro@gmail.com>
2024-11-03 15:27:05 +0100

    add hangman urls

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from . import views
-from .views import cemantix_game
+from .views import cemantix_game, hangman_game
 
 urlpatterns = [
     path('', views.games_page, name='games_page'),
     path('cemantix', cemantix_game, name='cemantix_game'),
+    path('hangman', hangman_game, name='hangman_game'),
 ]
