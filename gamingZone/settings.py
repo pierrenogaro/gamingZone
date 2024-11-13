dafac42a9bf7f16e9bc1eed57ea94d8e78ed520d
Pierre <pierre.nogaro@gmail.com>
2024-11-13 19:47:30 +0100

    maj

--- gamingZone/settings.py
+++ gamingZone/settings.py
@@ -78,10 +78,16 @@ WSGI_APPLICATION = 'gamingZone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gamingzone',
+        'USER': 'pierre',
+        'PASSWORD': 'vhHhV29arb7767@127.0.',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
